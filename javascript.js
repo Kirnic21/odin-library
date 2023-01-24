7498b4b2faca0fc5e88754ff2f53b24638e263ca
Kirnic21 <marcelokenji12121@gmail.com>
2023-01-24 20:33:46 -0300

    start card, done forms

--- javascript.js
+++ javascript.js
@@ -1,47 +1,52 @@
-let bookPlaceholder= new Book("asd","asda","dsa","Dsad")
-let bookPlaceholder2= new Book("asdss","assdada","dsasaa","sadsaDsad")
-let library=[bookPlaceholder,bookPlaceholder2]
-const button = document.querySelector("#add-book")
 
-function Book(name,author,pages, read){
-//constructor
-    this.name=name;
-    this.author=author;
-    this.pages = pages;
-    this.read = read;
-}
-function addBooktoLibrary(newBook,title,author,pages,read)
+const bookPlaceholder = new Book('asd', 'asda', 'dsa', 'Dsad');
+const bookPlaceholder2 = new Book('asdss', 'assdada', 'dsasaa', 'sadsaDsad');
+const library = [bookPlaceholder, bookPlaceholder2];
+// modal
+const modal = document.getElementById("forms-modal");
+const btn = document.getElementById("myBtn")
+const span = document.getElementsByClassName("close")[0];
+//cards
+const container = document.querySelector(".card-container")
+
+function createCards()
 {
-    newBook = new Book(title,author,pages,read);
-    newBook.name = document.querySelector("#title").value;
-    newBook.author = document.querySelector("#author").value;
-    newBook.pages = document.querySelector("#pages").value;
-    newBook.read = document.querySelector("#read").value;
-    library.push(newBook)
-    console.log(library)
+  let newCard=document.createElement("div")
+  newCard.classList.add("cards")
+  for(key in library){
+  console.log(library[key])
+  container.appendChild(newCard)
+  newCard.appendChild(text)
+  }
 }
-function generateTableHead(table)
-{
-    let thead = table.createTHead();
-    let row = thead.insertRow();
-    for (let key of data) {
-        let th = document.createElement("th");
-        let text = document.createTextNode(key);
-        th.appendChild(text);
-        row.appendChild(th);
-    }
+
+function Book(name, author, pages, read) {
+// constructor
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
-function generateTable(table,data){
-    for(let element of data){
-        let row = table.insertRow();
-        for (key in element){
-            let cell = row.insertCell();
-            let text =document.createTextNode(element[key]);
-            cell.appendChild(text)
-        }
-    }
+
+function addBooktoLibrary(newBook, title, author, pages, read) {
+  newBook = new Book(title, author, pages, read);
+  newBook.name = document.querySelector('#title').value;
+  newBook.author = document.querySelector('#author').value;
+  newBook.pages = document.querySelector('#pages').value;
+  newBook.read = document.querySelector('#read').value;
+  library.push(newBook);
+  console.log(library);
 }
-let table = document.querySelector(".Books")
-let data = Object.keys(library[1])
-generateTable(table,library);
-generateTableHead(table,data);
+
+btn.onclick = function(){
+  modal.style.display = "block"
+}
+span.onclick = function(){
+  modal.style.display = "none"
+}
+window.onclick = function(event){
+  if (event.target == modal){
+    modal.style.display = "none"
+  }
+}
+createCards(0)
