b70dfca97260a0d2ad97e1287384075e1ac015d7
Kirnic21 <marcelokenji12121@gmail.com>
2023-01-27 14:37:02 -0300

    Done read or not read button

--- javascript.js
+++ javascript.js
@@ -1,6 +1,5 @@
-const book1 = new Book("1984","Jeff Kinney","123","yes")
-const book2 = new Book("1985","George Pig","322","yes")
-const library = [book1];
+
+const library = [];
 // modal
 const modal = document.getElementById("forms-modal");
 const btn = document.getElementById("myBtn")
@@ -14,26 +13,27 @@ submit.addEventListener("click",function(){
   const title= document.querySelector("#title").value
   const author= document.querySelector("#author").value
   const pages = document.querySelector("#pages").value
-  const read = document.querySelector("#read").value
-  let newBook = new Book(title,author,pages,read)
+  const read =document.querySelector("#red")
+  let newBook = new Book(title,author,pages)
   createCard(newBook)
 })
-function Book(Name, Author, Pages, Read,Index) {
+function Book(Name, Author, Pages, Read) {
 // constructor
   this.Name = Name;
   this.Author = Author;
   this.Pages = Pages;
-  this.Read = Read;
 }
+
 function createCard(newestBook)
 {
   library.push(newestBook)
-  let newbookIndex = library.length - 1
+  let newbookIndex = library.length - 1;
   const newCard=document.createElement("div")
     newCard.classList.add("cards")
     const para= document.createElement("p")
     para.classList.add("para")
-  console.log(newbookIndex)
+    newCard.dataset.id=newbookIndex
+    console.log(newCard.dataset.id)
   let newValues = Object.entries(library[newbookIndex])
   for(let i in newValues)
   {
@@ -45,33 +45,34 @@ function createCard(newestBook)
   }
   container.appendChild(newCard)
   newCard.appendChild(para);
+  let removeButton= document.createElement("button")
+  removeButton.textContent="Remove"
+  newCard.appendChild(removeButton)
+  removeButton.addEventListener("click",function(){
+    delete library[newbookIndex];
+    newCard.remove()
+    newbookIndex=newbookIndex - 1
+    console.log(library)
+    console.log(newbookIndex)
+  })
+ const read= document.createElement("button")
+ read.textContent="Not Read"
+ let haveRead = false;
+ read.addEventListener("click",function(){
+  if(haveRead===false)
+  {
+    haveRead = true
+    read.textContent ="Read"
+  }
+  else if(haveRead === true)
+  {
+    haveRead = false
+    read.textContent = "Not read"
+  }
+ })
+ newCard.appendChild(read)
 }
 
-function showCards(){
-  for(let key in library)
-  {
-    //create a new card with a dataset id
-    const newCard=document.createElement("div")
-    newCard.classList.add("cards")
-    const para= document.createElement("p")
-    para.classList.add("para")
-    let values = Object.entries(library[key])
-    for(let i in values)
-    {
-      const head = document.createElement("h1")
-      let string = values[i].join(":")
-      let text2= document.createTextNode(string)
-      para.appendChild(head)
-      head.appendChild(text2)
-    }
-    container.appendChild(newCard)
-    newCard.appendChild(para);
-    //remove button
-    const removeButton = document.createElement("button");
-    removeButton.textContent="remove"
-    newCard.appendChild(removeButton);
-  }
-}
 btn.onclick = function(){
   modal.style.display = "block"
 }
